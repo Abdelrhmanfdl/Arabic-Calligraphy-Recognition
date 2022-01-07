488832ddb5646c609e7593b98e75ceb575185271
AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>
2022-01-07 19:57:16 +0200

    output files update
    removed extra new line

--- predict.py
+++ predict.py
@@ -26,6 +26,7 @@ filename = f'model.sav'
 model = pickle.load(open(filename, 'rb'))
 results = open(out_dirct+'results.txt', 'w')
 times = open(out_dirct+'times.txt', 'w')
+index = 0
 for filename in sorted(glob.glob(test_dirct+"*.png")):
     start = time()
     try:
@@ -35,18 +36,28 @@ for filename in sorted(glob.glob(test_dirct+"*.png")):
         data , edges = preprocessing.pre_processing(data)
         data_features = features.get_features(data,edges)
         test_predictions = model.predict(data_features)
-        results.write(str(dictionary[test_predictions[0]])+"\n")
+        prediction = str(dictionary[test_predictions[0]])
+        if index != 0:
+            prediction = "\n"+str(prediction) 
+        results.write(prediction)
         end = time()
         time_taken = end - start
         if time_taken == 0.0:
             time_taken = (1/1000)
-        times.write(str(time_taken)+"\n")
+        time_taken = str(time_taken) 
+        if index != 0:
+            time_taken = "\n"+str(time_taken) 
+        times.write(time_taken)
     except:
-        results.write("-1\n")
+        prediction = "-1"
+        if index != 0:
+            prediction = "\n"+prediction
+        results.write(prediction)
         end = time()
-        time_taken = end - start
-        if time_taken == 0.0:
-            time_taken = (1/1000)
-        times.write(str(time_taken)+"\n")
+        time_taken = str(time_taken) 
+        if index != 0:
+            time_taken = "\n"+str(time_taken) 
+        times.write(time_taken)
+    index += 1
     
     
